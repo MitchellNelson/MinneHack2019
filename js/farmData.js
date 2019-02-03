9d9d911356ba2f9ea16b84ec9c26b81bb280dbc1
Peter Farley <far.peter1@gmail.com>
2019-02-02 21:38:11 -0600

    Added function to get owner or plot by ID.

--- js/farmData.js
+++ js/farmData.js
@@ -5,11 +5,15 @@
 $(document).ready(function() {
     initFarmData();
     // TODO: Run through plots and add markers
+    for(var i = 1; i <= plots.length; i++) {
+        console.log(i + ": " + getPlot(i));
+    }
 });
 
-var owners = null;
-var plants = null;
-var plots  = null;
+var owners   = null;
+var plants   = null;
+var plots    = null;
+var datalogs = null;
 
 function initFarmData() {
     jQuery.ajaxSetup({async:false});
@@ -29,9 +33,28 @@ function initFarmData() {
         plots = data;
     }, "json");
 
+    $.get('data/datalogs.json', function(data) {
+        console.log(data);
+        datalogs = data;
+    }, "json");
+
     jQuery.ajaxSetup({async:true});
 }
 
 function getOwner(id) {
+    for(i in owners) {
+        if(owners[i].id === id) {
+            return owners[i];
+        }
+    }
+    return null;
+}
 
+function getPlot(id) {
+    for(i in plots) {
+        if(plots[i].id === id) {
+            return plots[i];
+        }
+    }
+    return null;
 }

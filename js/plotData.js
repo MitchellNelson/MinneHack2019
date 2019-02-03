e70aa10b17b035394a24ba8facce4a683b1bca76
unknown <sore1324@stthomas.edu>
2019-02-03 00:16:13 -0600

    plotData onReady work

--- js/plotData.js
+++ js/plotData.js
@@ -17,10 +17,18 @@ $(document).ready(() => {
     var ownerName = getOwner(plot.owner).name;
     var plantName = getPlant(plot.plant).name;
     // TODO: Display farm data
+    console.log(ownerName);
+    console.log(plantName);
 
-    var logs = getPlotLogs(plotID);
+    // Get and sort logs
+    var logs = getPlotLogs(+plotID);
+    logs.sort((a, b) => {
+        return new Date(a.date) > new Date(b.date) ? -1 : 1;
+    });
 
+    // Do stuff for each plot log
     logs.forEach(entry => {
         // TODO: Display log entry
+        console.log(entry);
     });
 });

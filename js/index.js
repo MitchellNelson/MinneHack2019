f95c3b0757d568b8167a69b5aa53ec5195fbfd07
Riley Tschumper <riley.tschumper@gmail.com>
2019-02-02 21:22:39 -0600

    recentered map

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-    });
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 44.814, lng: -94.95883 },
+    zoom: 12
+  });
+  var testLocation = { lat: 44.814, lng: -94.95883 };
+  var marker = new google.maps.Marker({ position: testLocation, map: map });
 }

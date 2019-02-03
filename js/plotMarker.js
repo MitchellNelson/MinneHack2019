f276bf060b37a78ba0dea7f61092bdc6ca226b2c
unknown <sore1324@stthomas.edu>
2019-02-02 23:24:52 -0600

    Sort pre-image grab

--- js/plotMarker.js
+++ js/plotMarker.js
@@ -10,26 +10,23 @@ function makePlotMarker(position, mapId, title, id) {
   return this;
 }
 
-function onPlotMarkerClick() {
-  console.log("onPlotMarkerClick id: " + this.id);
-  console.log(this);
+function onPlotMarkerClick(){
+    console.log('onPlotMarkerClick id: ' + this.id);
+    console.log(this);
 
-  plot = getPlot(this.id);
-  var plotInfo = getPlotInfo(plot);
-  setOverlayFields(plotInfo);
+    plot = getPlot(this.id);
+    var plotInfo = getPlotInfo(plot);
 
-  // Get all plot logs associated with this plot.
-  var plotLogs = getPlotLogs(plot.id);
-  // Sort the logs from newest to oldest
-  plotLogs.sort((a, b) => {
-    aDate = new Date(a.date);
-    bDate = new Date(b.date);
-    return aDate > bDate ? -1 : 1;
-  });
-  console.log("plot logs sorted");
-  console.log(plotLogs);
+    // Sort the logs from newest to oldest
+    plotInfo.data.sort((a,b) => {
+        aDate = new Date(a.date);
+        bDate = new Date(b.date);
+        return aDate > bDate ? -1 : 1;
+    });
 
-  $("#overlay").show();
+    setOverlayFields(plotInfo);
+
+    $("#overlay").show();
 }
 
 function getPlotInfo(plot) {
@@ -46,8 +43,8 @@ function setOverlayFields(plotInfo) {
   document.getElementById("overlay-crop").innerHTML = plotInfo.crop;
   document.getElementById("overlay-owner").innerHTML = plotInfo.owner;
   //console.log(plotInfo.data);
-  console.log(plotInfo.data[plotInfo.data.length - 1].data.photos[0]);
+  console.log(plotInfo.data[0].data.photos[0]);
   document.getElementById("overlay-image").src =
-    plotInfo.data[plotInfo.data.length - 1].data.photos[0];
+    plotInfo.data[0].data.photos[0];
   //imageDiv.appendChild(plotInfo.data[plotInfo.data.length - 1].data.photos[0]);
 }

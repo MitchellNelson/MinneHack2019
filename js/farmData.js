d6b49521de487ecdc033f9b5e19109dccc6f95d1
Riley Tschumper <riley.tschumper@gmail.com>
2019-02-02 23:13:55 -0600

    added images to overlay

--- js/farmData.js
+++ js/farmData.js
@@ -7,78 +7,97 @@
 });*/
 
 function placePlotMarkers() {
-    plots.forEach(plot => {
-        makePlotMarker(
-            { lat: plot.location[0],
-              lng: plot.location[1] },
-            map, "TODO", plot.id);
-    });
+  plots.forEach(plot => {
+    makePlotMarker(
+      { lat: plot.location[0], lng: plot.location[1] },
+      map,
+      "TODO",
+      plot.id
+    );
+  });
 }
 
-var owners   = null;
-var plants   = null;
-var plots    = null;
+var owners = null;
+var plants = null;
+var plots = null;
 var datalogs = null;
 
 function initFarmData() {
-    jQuery.ajaxSetup({async:false});
+  jQuery.ajaxSetup({ async: false });
 
-    $.get('data/owners.json', function(data) {
-        console.log(data);
-        owners = data;
-    }, "json");
+  $.get(
+    "data/owners.json",
+    function(data) {
+      console.log(data);
+      owners = data;
+    },
+    "json"
+  );
 
-    $.get('data/plants.json', function(data) {
-        console.log(data);
-        plants = data;
-    }, "json");
+  $.get(
+    "data/plants.json",
+    function(data) {
+      console.log(data);
+      plants = data;
+    },
+    "json"
+  );
 
-    $.get('data/plots.json', function(data) {
-        console.log(data);
-        plots = data;
-    }, "json");
+  $.get(
+    "data/plots.json",
+    function(data) {
+      console.log(data);
+      plots = data;
+    },
+    "json"
+  );
 
-    $.get('data/datalogs.json', function(data) {
-        console.log(data);
-        datalogs = data;
-    }, "json");
+  $.get(
+    "data/datalogs.json",
+    function(data) {
+      console.log(data);
+      datalogs = data;
+    },
+    "json"
+  );
 
-    jQuery.ajaxSetup({async:true});
+  jQuery.ajaxSetup({ async: true });
 }
 
 function getOwner(id) {
-    for(i in owners) {
-        if(owners[i].id === id) {
-            return owners[i];
-        }
+  for (i in owners) {
+    if (owners[i].id === id) {
+      return owners[i];
     }
-    return null;
+  }
+  return null;
 }
 
 function getPlot(id) {
-    for(i in plots) {
-        if(plots[i].id === id) {
-            return plots[i];
-        }
+  for (i in plots) {
+    if (plots[i].id === id) {
+      return plots[i];
     }
-    return null;
+  }
+  return null;
 }
 
 function getPlant(id) {
-    for(i in plants) {
-        if(plants[i].id === id) {
-            return plants[i];
-        }
+  for (i in plants) {
+    if (plants[i].id === id) {
+      return plants[i];
     }
-    return null;
+  }
+  return null;
 }
 
 function getPlotLogs(id) {
-    var logs = Array();
+  var logs = Array();
 
-    for(i in datalogs) {
-        if(datalogs[i].plot === id) {
-            logs.push(datalogs[i]);
-        }
+  for (i in datalogs) {
+    if (datalogs[i].plot === id) {
+      logs.push(datalogs[i]);
     }
+  }
+  return logs;
 }
--- js/plotMarker.js
+++ js/plotMarker.js
@@ -1,37 +1,42 @@
+function makePlotMarker(position, mapId, title, id) {
+  var thisMarker = new google.maps.Marker({
+    position: position,
+    map: mapId,
+    title: title,
+    id: id
+  });
 
-function makePlotMarker(position, mapId, title, id){
-    var thisMarker = new google.maps.Marker({
-        position: position,
-        map: mapId,
-        title: title,
-        id: id,
-    })
-
-    thisMarker.addListener('click', onPlotMarkerClick);
-    return this;
+  thisMarker.addListener("click", onPlotMarkerClick);
+  return this;
 }
 
-function onPlotMarkerClick(){
-    console.log('onPlotMarkerClick id: ' + this.id);
-    console.log(this);
-    $("#overlay").show();
+function onPlotMarkerClick() {
+  console.log("onPlotMarkerClick id: " + this.id);
+  console.log(this);
+  $("#overlay").show();
 
-    plot = getPlot(this.id);
+  plot = getPlot(this.id);
 
-    var plotInfo = getPlotInfo(plot);
-    setOverlayFields(plotInfo);
+  var plotInfo = getPlotInfo(plot);
+  setOverlayFields(plotInfo);
 }
 
-function getPlotInfo(plot){
-    var plotInfo = {
-        id: plot.id,
-        crop: getPlant(plot.plant).name,
-        owner: getOwner(plot.owner).name,
-    }
-    return plotInfo;
+function getPlotInfo(plot) {
+  var plotInfo = {
+    id: plot.id,
+    crop: getPlant(plot.plant).name,
+    owner: getOwner(plot.owner).name,
+    data: getPlotLogs(plot.id)
+  };
+  return plotInfo;
 }
 
-function setOverlayFields(plotInfo){
-    document.getElementById("overlay-crop").innerHTML = plotInfo.crop;
-    document.getElementById("overlay-owner").innerHTML = plotInfo.owner;
+function setOverlayFields(plotInfo) {
+  document.getElementById("overlay-crop").innerHTML = plotInfo.crop;
+  document.getElementById("overlay-owner").innerHTML = plotInfo.owner;
+  //console.log(plotInfo.data);
+  console.log(plotInfo.data[plotInfo.data.length - 1].data.photos[0]);
+  document.getElementById("overlay-image").src =
+    plotInfo.data[plotInfo.data.length - 1].data.photos[0];
+  //imageDiv.appendChild(plotInfo.data[plotInfo.data.length - 1].data.photos[0]);
 }

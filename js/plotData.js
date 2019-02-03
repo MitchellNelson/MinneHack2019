f764b1e376777d4bc1b5dc034aceb5c057013fba
unknown <sore1324@stthomas.edu>
2019-02-03 00:59:49 -0600

    Templating plotData.js

--- js/plotData.js
+++ js/plotData.js
@@ -1,3 +1,5 @@
+contentTemplate = $(".template");
+
 function getGetParam(name) {
     var url = new URL(window.location);
     return url.searchParams.get(name);
@@ -29,6 +31,35 @@ $(document).ready(() => {
     // Do stuff for each plot log
     logs.forEach(entry => {
         // TODO: Display log entry
+        console.log("calling new snapshot with " + entry.date);
+        var newCard = makeNewSnapshotTemplate(entry.date)
+        newCard.show();
+        console.log("attempting to append card...");
+        console.log(newCard);
+        //addExistingFieldsToSnapshotCard(newCard, entry.data);
+        $("#content").append(newCard);
         console.log(entry);
     });
 });
+
+function makeNewSnapshotTemplate(date) {
+    var card = contentTemplate.clone();
+    var cardDate = card.find(".snapshot-date");
+    var dateField = $("<span/>");
+    dateField.text("Date: " + date);
+    cardDate.append(dateField);
+    card.append(cardDate);
+    console.log("Returning card:");
+    console.log(card);
+    return card;
+}
+
+function addExistingFieldsToSnapshotCard(card, data){
+    if (data.photos != null && data.photos.length > 0){
+        var img = $("<img/>");
+        img.src = data.photos[0];
+        card.append(img);
+    }
+    if (data.seedsPlanted != null){
+    }
+}

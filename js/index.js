663a9c4afd39b8f5a766ec884c7db9aea5ed039a
Peter Farley <far.peter1@gmail.com>
2019-02-03 11:26:47 -0600

    Changed default map zoom, view, and center.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 44.814, lng: -94.95883 },
-    zoom: 12
+    center: { lat: 44.8, lng: -94.99 },
+    zoom: 14,
+    mapTypeId: 'satellite'  
   });
 
   initFarmData();
